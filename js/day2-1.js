310733c1175674597f8a4e90ce6f6a79f09f241e
weiensong <wes@shoot.net.cn>
2024-02-22 11:04:17 +0800

    feat: 2-1

--- js/day2-1.js
+++ js/day2-1.js
@@ -6,21 +6,22 @@ document.write('<h1>' + content + '</h1>')
 let size = prompt('random size int: ')
 
 let randomNumbers = [];
-for (let i = 0; i <= size ; i++){
-  let randomNumber = Math.floor(Math.random() * 100) + 1;
+for (let i = 0; i < size; i++) {
+  let randomNumber = Math.floor(Math.random() * 1000) + 1;
   randomNumbers.push(randomNumber);
 }
 console.log(randomNumbers);
 
-for (let i = 0; i < randomNumbers.length; i++){
-  for (let j = 0; j < randomNumbers.length; j++){
-   if (randomNumbers[j] > randomNumbers[j + 1]){
+for (let i = 0; i < randomNumbers.length; i++) {
+  for (let j = 0; j < randomNumbers.length; j++) {
+    if (randomNumbers[j] > randomNumbers[j + 1]) {
       let temp = randomNumbers[j];
       randomNumbers[j] = randomNumbers[j + 1];
-      randomNumbers[j+1] = temp;
+      randomNumbers[j + 1] = temp;
     }
   }
 }
-console.log(randomNumbers)
+console.log(randomNumbers);
+console.log(randomNumbers.length);
 
-document.write('<span>'+ randomNumbers + '</span>')
+document.write('<span>' + randomNumbers + '</span>');
